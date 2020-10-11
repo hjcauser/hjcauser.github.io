65796922b278e9b7e778e19ddfadf47f35516515
hjcauser <51858840+hjcauser@users.noreply.github.com>
2020-10-11 18:17:42 +1100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 
 
 app.get('/netatmo1', async (request, response) => {
-const netatmo_api1 = `https://api.netatmo.com/api/getstationsdata?access_token=5f67f50c4820657f86439699|024dc3ceb49bfde5853d232004f3f7a0&device_id=70:ee:50:03:b7:9c`
+const netatmo_api1 = `https://api.netatmo.com/api/getstationsdata?access_token={insertaccesstoken}&device_id=70:ee:50:03:b7:9c`
 const netat_response = await fetch(netatmo_api1);
 const json = await netat_response.json();
 response.json(json);
